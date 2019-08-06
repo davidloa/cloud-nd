d4059fb628ae464444d1c61b73c31e977f16a41b
David Loa <david.loa1985@gmail.com>
2019-08-06 21:17:41 +0800

    added dockerfile to each services

--- c3/c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
+++ c3/c3-restapi-feed/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,9 +1,32 @@
 import { Router, Request, Response } from 'express';
 import { FeedItem } from '../models/FeedItem';
-import { requireAuth } from '../../users/routes/auth.router';
+import { NextFunction } from 'connect';
+import * as jwt from 'jsonwebtoken';
 import * as AWS from '../../../../aws';
+import * as c from '../../../../config/config';
 
 const router: Router = Router();
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+ //   return next();
+     if (!req.headers || !req.headers.authorization){
+         return res.status(401).send({ message: 'No authorization headers.' });
+     }
+
+
+     const token_bearer = req.headers.authorization.split(' ');
+     if(token_bearer.length != 2){
+         return res.status(401).send({ message: 'Malformed token.' });
+     }
+
+     const token = token_bearer[1];
+     return jwt.verify(token, c.config.jwt.secret , (err, decoded) => {
+       if (err) {
+         return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
+       }
+       return next();
+     });
+ }
 
 // Get all feed items
 router.get('/', async (req: Request, res: Response) => {
@@ -17,7 +40,7 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 // Get a specific resource
-router.get('/:id', 
+router.get('/:id',
     async (req: Request, res: Response) => {
     let { id } = req.params;
     const item = await FeedItem.findByPk(id);
@@ -25,8 +48,8 @@ router.get('/:id',
 });
 
 // update a specific resource
-router.patch('/:id', 
-    requireAuth, 
+router.patch('/:id',
+    requireAuth,
     async (req: Request, res: Response) => {
         //@TODO try it yourself
         res.send(500).send("not implemented")
@@ -34,19 +57,19 @@ router.patch('/:id',
 
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName', 
-    requireAuth, 
+router.get('/signed-url/:fileName',
+    requireAuth,
     async (req: Request, res: Response) => {
     let { fileName } = req.params;
     const url = AWS.getPutSignedUrl(fileName);
     res.status(201).send({url: url});
 });
 
-// Post meta data and the filename after a file is uploaded 
+// Post meta data and the filename after a file is uploaded
 // NOTE the file name is they key name in the s3 bucket.
 // body : {caption: string, fileName: string};
-router.post('/', 
-    requireAuth, 
+router.post('/',
+    requireAuth,
     async (req: Request, res: Response) => {
     const caption = req.body.caption;
     const fileName = req.body.url;
--- c3/c3-restapi-user/src/controllers/v0/users/routes/auth.router.ts
+++ c3/c3-restapi-user/src/controllers/v0/users/routes/auth.router.ts
@@ -28,17 +28,17 @@ function generateJWT(user: User): string {
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
- //  return next();
+//   return next();
     if (!req.headers || !req.headers.authorization){
         return res.status(401).send({ message: 'No authorization headers.' });
     }
-
+    
 
     const token_bearer = req.headers.authorization.split(' ');
     if(token_bearer.length != 2){
         return res.status(401).send({ message: 'Malformed token.' });
     }
-
+    
     const token = token_bearer[1];
     return jwt.verify(token, c.config.jwt.secret , (err, decoded) => {
       if (err) {
@@ -48,8 +48,8 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
     });
 }
 
-router.get('/verification',
-    requireAuth,
+router.get('/verification', 
+    requireAuth, 
     async (req: Request, res: Response) => {
         return res.status(200).send({ auth: true, message: 'Authenticated.' });
 });
